9c31624b822d40072f3dd861a4777873b0c77815
Carlos Hinostroza <chinostroza@gmail.com>
2026-07-02 19:27:15 -0400

    fix: add generated protobuf files for gRPC communication

--- cerebelum/proto/__init__.py
+++ cerebelum/proto/__init__.py
@@ -1,22 +1,2 @@
-"""Generated gRPC protocol buffers."""
 from .worker_service_pb2 import *
 from .worker_service_pb2_grpc import *
-
-__all__ = [
-    "WorkerServiceStub",
-    "RegisterRequest",
-    "RegisterResponse",
-    "HeartbeatRequest",
-    "HeartbeatResponse",
-    "UnregisterRequest",
-    "PollRequest",
-    "Task",
-    "TaskResult",
-    "TaskStatus",
-    "ErrorInfo",
-    "Ack",
-    "Blueprint",
-    "BlueprintValidation",
-    "ExecuteRequest",
-    "ExecutionHandle",
-]
